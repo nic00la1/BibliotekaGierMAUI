454a4316de1a63518a3d1b589ad353b4c97026aa
nic00la <nic00la.kaleta@gmail.com>
2024-03-16 18:52:18 +0100

    It works

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,10 +13,15 @@ namespace BibliotekaGierMAUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("NotoSerif-Bold.ttf", "NotoSerifBold");
+                    fonts.AddFont("Poppins-Bold.ttf", "PoppinsBold");
+                    fonts.AddFont("Poppins-SemiBold.ttf", "PoppinsSemibold");
+                    fonts.AddFont("Poppins-Regular.ttf", "Poppins");
+                    fonts.AddFont("MaterialIconsOutlined-Regular.otf", "Material");
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
