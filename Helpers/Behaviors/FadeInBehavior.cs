d0403899b930a5a7f29e992014454ca4c94ea812
nic00la <nic00la.kaleta@gmail.com>
2024-03-18 16:22:20 +0100

    FadeInBehavior in Tag ✨

---
+++ Helpers/Behaviors/FadeInBehavior.cs
@@ -0,0 +1,24 @@
+﻿namespace BibliotekaGierMAUI.Helpers.Behaviors
+{
+    public class FadeInBehavior : Behavior<View>
+    {
+        protected override void OnAttachedTo(View bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.BindingContextChanged += OnBindingContextChanged;
+        }
+
+        protected override void OnDetachingFrom(View bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.BindingContextChanged -= OnBindingContextChanged;
+        }
+
+        private async void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            var view = (View)sender;
+            view.Opacity = 0;
+            await view.FadeTo(1, 1000);
+        }
+    }
+}
--- MVVM/Views/HomePage.xaml.cs
+++ MVVM/Views/HomePage.xaml.cs
@@ -10,4 +10,12 @@ public partial class HomePage : ContentPage
         InitializeComponent();
         this.BindingContext = new HomeViewModel(games);
     }
+
+    private async void ShowAllTags_Animation(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        await button.ScaleTo(0.95, 50, Easing.CubicOut);
+        await button.ScaleTo(1, 50, Easing.CubicIn);
+    }
+
 }
