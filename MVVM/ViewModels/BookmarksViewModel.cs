74949e9ec0c6784aa2a29f1d8a67b05f862509b2
nic00la <nic00la.kaleta@gmail.com>
2024-03-18 17:34:07 +0100

    Dodano przechodzenie do gry z poziomu Bookmarks

--- MVVM/ViewModels/BookmarksViewModel.cs
+++ MVVM/ViewModels/BookmarksViewModel.cs
@@ -8,8 +8,18 @@ namespace BibliotekaGierMAUI.MVVM.ViewModels
         public BookmarksViewModel(IGamesService gamesService)
         {
             Games = gamesService.GetBookmarkedGames();
+
+            TappedCommand = new Command<Game>((game) =>
+            {
+                var query = new Dictionary<string, object>()
+                {
+                    { "game", game }
+                };
+                Shell.Current.GoToAsync("//bookmarks/game", query);
+            });
         }
 
         public ICollection<Game> Games { get; set; }
+        public Command TappedCommand { get; set; }
     }
 }
