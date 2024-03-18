d56e3d541f8165601f933531ea6e55e00673614f
nic00la <nic00la.kaleta@gmail.com>
2024-03-18 09:14:36 +0100

    Category UI

---
+++ Helpers/GameTemplateSelector.cs
@@ -0,0 +1,15 @@
+﻿using BibliotekaGierMAUI.Models;
+
+namespace BibliotekaGierMAUI.Helpers
+{
+    public class GameTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate? EvenTemplate { get; set; }
+        public DataTemplate? OddTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((Game)item).Id % 2 == 0 ? EvenTemplate : OddTemplate;
+        }
+    }
+}
